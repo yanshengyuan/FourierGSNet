0c648a3905b24ed989d5d1502fffedee9b18806e
Shengyuan Yan <39942337+yanshengyuan@users.noreply.github.com>
2025-07-18 23:18:31 +0200

    Add files via upload

---
+++ Benchmarked_Phase_Retrieval_Methods/Complex_forward_path-PBF-LBM_Laser_BeamShaping-InShaPe/deepcdi_ComplexInShaPe/run.sh
@@ -0,0 +1,7 @@
+train and subsequent test:
+python3 0_train-cnn/train_cnn_chair.py
+python3 0_train-cnn/train_cnn_tear.py
+python3 0_train-cnn/train_cnn_ring.py
+python3 0_train-cnn/train_cnn_hat.py
+python3 0_train-cnn/train_cnn_gaussian.py
+python3 0_train-cnn/train_cnn_rec.py
