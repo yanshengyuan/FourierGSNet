471e49009e804bb06894620648ecc40a4dee5b91
Shengyuan Yan <39942337+yanshengyuan@users.noreply.github.com>
2025-07-18 23:30:18 +0200

    Delete Benchmarked_Phase_Retrieval_Methods/Complex_forward_path-PBF-LBM_Laser_BeamShaping-InShaPe/FourierGSNet_ComplexInShaPe directory

--- Benchmarked_Phase_Retrieval_Methods/Complex_forward_path-PBF-LBM_Laser_BeamShaping-InShaPe/FourierGSNet_ComplexInShaPe/run.sh
+++
@@ -1,8 +0,0 @@
-python3 train_GSNet.py --data ../InShaPe_dataset/denserec30k_pre --epochs 30 --batch_size 2 --gpu 1 --lr 0.0002 --step_size 2 --seed 22 --pth_name GSNet_rec.pth.tar --val_vis_path GSNet_rec
-
-python3 train_reg.py --data ../InShaPe_dataset/denserec30k_pre --epochs 30 --batch_size 2 --gpu 1 --lr 0.0002 --step_size 2 --seed 22 --pth_name reg_rec.pth.tar --val_vis_path reg_rec
-
-eval:
-python3 train_reg.py --data ../InShaPe_dataset/denserec30k_pre --batch_size 2 --gpu 0 --seed 22 --pth_name reg_rec.pth.tar --val_vis_path reg_rec --eval
-
-python3 train_GSNet.py --data ../InShaPe_dataset/denserec30k_pre --batch_size 2 --gpu 0 --seed 22 --pth_name GSNet_rec.pth.tar --val_vis_path GSNet_rec --eval
